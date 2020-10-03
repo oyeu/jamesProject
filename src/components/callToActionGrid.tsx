56f0ab499cbe20f3191f41be270693267e33ed87
oyeu <daarangovi@gmail.com>
2020-10-02 22:00:49 -0500

    ajuste titutlo del call to action en la pagina de inicio

--- src/components/callToActionGrid.tsx
+++ src/components/callToActionGrid.tsx
@@ -6,12 +6,20 @@ import CallToActionBlock from "./callToActionBlock"
 const CallToActionGridWrapper = styled.section`
   max-width: 800px;
   margin: 0 auto;
+
+  .call_to_action_section_title {
+    color:white;
+    text-align:center;
+    margin: 20px auto;
+  }
 `
 
 const CallToActionGrid = ({ title, callToActions }: any) => {
   return (
     <CallToActionGridWrapper>
-      <RichText render={title} />
+      <div className="call_to_action_section_title">
+        <RichText render={title} />
+      </div>
       {callToActions.map((callToAction: any, i: any) => {
         return (
           <CallToActionBlock
--- src/pages/contact-success.tsx
+++ src/pages/contact-success.tsx
@@ -1,10 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from "styled-components"
+
+const PageWrapper = styled.div`
+    text-align:center;
+    max-width:800px;
+    margin: 0 auto;
+    color:white;
+`
 
 const ContactSuccess = () => {
   return (
     <Layout>
-      <h1>¡Gracias or comunicarte con nosotros!</h1>
+        <PageWrapper>
+            <h1>¡Gracias por comunicarte con nosotros!</h1>
+        </PageWrapper>
     </Layout>
   )
 }
