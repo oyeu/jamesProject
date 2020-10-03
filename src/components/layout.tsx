be099ab8ab209168d7f4b5716f902f0917661af3
oyeu <daarangovi@gmail.com>
2020-10-03 04:01:22 -0500

    Changes in header: separated branding and subranding, display logo image before branding, change brandings and navlinks colors tu blue neon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,6 +18,7 @@ const navigationQuery = graphql`
           node {
             branding
             logo
+            subranding
             navigation_links {
               label
               link {
@@ -58,11 +59,12 @@ const NavLink = styled.div`
     text-decoration: none;
     font-weight: bold;
     font-size: 16px;
-    text-shadow: 0 0 5px rgba(248, 5, 47, 1), 0 0 10px rgba(248, 5, 47, 1),
-      0 0 20px rgba(248, 5, 47, 1), 0 0 40px rgba(136, 14, 59, 1),
-      0 0 80px rgba(136, 14, 59, 1), 0 0 90px rgba(136, 14, 59, 1),
-      0 0 100px rgba(136, 14, 59, 1), 0 0 140px rgba(136, 14, 59, 1),
-      0 0 180px rgba(136, 14, 59, 1);
+    
+    text-shadow: 0 0 5px rgba(0, 178, 255, 1), 0 0 10px rgba(0, 178, 255, 1),
+    0 0 20px rgba(0, 178, 255, 1), 0 0 40px rgba(38, 104, 127, 1),
+    0 0 80px rgba(38, 104, 127, 1), 0 0 90px rgba(38, 104, 127, 1),
+    0 0 100px rgba(38, 104, 127, 1), 0 0 140px rgba(38, 104, 127, 1),
+    0 0 180px rgba(38, 104, 127, 1);
 
     &:hover {
       color: #fff;
@@ -83,16 +85,40 @@ const NavLinks = styled.div`
 `
 
 const Branding = styled.div`
-  margin: auto 0;
+  display: flex;
+  margin: auto 20px;
+  padding: 0 10px;
   font-weight: bold;
   color: #fff;
-  font-size: 20px;
+  font-size: 30px;
+
   text-shadow: 0 0 5px rgba(0, 178, 255, 1), 0 0 10px rgba(0, 178, 255, 1),
     0 0 20px rgba(0, 178, 255, 1), 0 0 40px rgba(38, 104, 127, 1),
     0 0 80px rgba(38, 104, 127, 1), 0 0 90px rgba(38, 104, 127, 1),
     0 0 100px rgba(38, 104, 127, 1), 0 0 140px rgba(38, 104, 127, 1),
     0 0 180px rgba(38, 104, 127, 1);
+  
+  .branding {
+  
+  }
+  
+  .subranding {
+    display: flex;
+    margin-left: 20px;
+    font-size: 16px;
+    content
+    
+  }
+  
 `
+
+/*Neon rojo
+  text-shadow: 0 0 5px rgba(248, 5, 47, 1), 0 0 10px rgba(248, 5, 47, 1),
+      0 0 20px rgba(248, 5, 47, 1), 0 0 40px rgba(136, 14, 59, 1),
+      0 0 80px rgba(136, 14, 59, 1), 0 0 90px rgba(136, 14, 59, 1),
+      0 0 100px rgba(136, 14, 59, 1), 0 0 140px rgba(136, 14, 59, 1),
+      0 0 180px rgba(136, 14, 59, 1);
+*/
 // const Logo = styled.img``
 
 interface Links {
@@ -104,6 +130,19 @@ interface Props {
   children: JSX.Element[] | JSX.Element
 }
 
+const LogoHeader = styled.div`
+  display:flex;
+
+  img{
+    border: 1px solid #ddd;
+    display:block;
+    margin:auto;
+    width:100%;
+    height:auto;
+    border-radius:20px;
+  }
+`
+
 const Layout = ({ children }: Props) => {
   return (
     <>
@@ -111,14 +150,23 @@ const Layout = ({ children }: Props) => {
         <StaticQuery
           query={navigationQuery}
           render={data => {
+            let headerContent = data.prismic.allNavigations.edges[0].node
             console.log(data)
             return (
               <>
+                <LogoHeader >
+                  <img src={headerContent.logo.url} alt='logo header'/>
+                </LogoHeader>
                 <Branding>
-                  {data.prismic.allNavigations.edges[0].node.branding}
+                  <div className='branding'>
+                    {headerContent.branding}
+                  </div> 
+                  <div className='subranding'>
+                    {headerContent.subranding}
+                  </div>
                 </Branding>
                 <NavLinks>
-                  {data.prismic.allNavigations.edges[0].node.navigation_links.map(
+                  {headerContent.navigation_links.map(
                     (link: Links) => {
                       if (link.link._meta.uid==="inicio") {
                         return(
